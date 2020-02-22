5305a7b4fc05afdd62273458c9b9952c49cd57f0
OlgaNikulina <nikulinaov@inbox.ru>
2020-02-22 11:41:11 +0500

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@ public class Main {
     public static void main(String[] args) {
       int before = 100;
       int bonus;
-      long amount = 1100;
-      int after = (int) (before + amount);
-      if (after > 1100) {
-          bonus = (int) (amount / 100);
+      int after = 1200;
+      int limit = 1000;
+      int refill = (after - before);
+      if (refill > limit) {
+          bonus = (refill / 100);
       } else {
           bonus = 0;
       }
-      long total = bonus + before + amount;
-        System.out.println(total);
+      int total = before + bonus + refill;
+        System.out.println(bonus);
     }
 }
