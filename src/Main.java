d6b09c505f395ea8086f0113fac6abc4ff9e890e
OlgaNikulina <nikulinaov@inbox.ru>
2020-02-22 19:23:49 +0500

    src/Main.java modified

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,15 @@ public class Main {
     public static void main(String[] args) {
       int before = 100;
       int bonus;
-      int after = 1200;
-      int limit = 1000;
-      int refill = (after - before);
-      if (refill > limit) {
+      int refill = 1100;
+      int treshold = 1000;
+      int after = (refill + before);
+      if (refill > treshold) {
           bonus = (refill / 100);
       } else {
           bonus = 0;
       }
-      int total = before + bonus + refill;
+      int total = bonus + after;
         System.out.println(bonus);
     }
 }
